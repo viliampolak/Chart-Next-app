cd92d628c7df111ee1afc105d092944a31c5ad27
vilia <viliam.polak11@gmail.com>
2023-10-29 20:23:36 +0100

    navbar design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Nav from './nav'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Nav/>
         {children}
         </body>
     </html>
--- src/app/test/layout.tsx
+++ src/app/test/layout.tsx
@@ -1,8 +1,7 @@
-
+import Nav from '../nav'
 export default function TestLayout({children}:{children: React.ReactNode}){
     return (
     <section>
-    <h1>Test layout</h1>
     {children}
     </section>
     )
